eeb8ce6c06e743736427bd72bfd4ebc30c2d14a4
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-03-21 15:00:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let tableBody = document.getElementById('table-body')
+    fetchRequest()
+    
+
+
+    function fetchRequest(){
+        fetch('http://localhost:3000/dogs')
+        .then(res => res.json())
+        .then(appendDom)
+    }
+    
+    function appendDom(dogsArray){
+        dogsArray.forEach(dog => {
+            const table = document.createElement('tr')
+            table.innerHTML = `
+            <tr>
+            <td>${dog.name}</td>
+            <td>${dog.breed}</td>
+            <td>${dog.sex}</td>
+            <td><button>Edit</button></td>
+            </tr> `
+            tableBody.append(table)
+            const editBtn = document.querySelector('button')
+            editBtn.addEventListener('click', () => {
+                console.log('You have clicked')
+            })
+        })
+    }
+    
 
 })
+
